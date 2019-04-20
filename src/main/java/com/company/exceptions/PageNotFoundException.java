807ef7af35b7bc3f132221d31a16fd68253b909a
PrimaryInspection <cplusgaming1@gmail.com>
2019-04-20 20:35:34 +0300

    added PageNotFound(404) exception handling

--- src/main/java/com/company/exceptions/PageNotFoundException.java
+++ src/main/java/com/company/exceptions/PageNotFoundException.java
@@ -1,8 +1,14 @@
 package com.company.exceptions;
 
+import com.company.utils.ConfigurationManager;
+
+import javax.servlet.RequestDispatcher;
+
 public class PageNotFoundException extends RuntimeException {
     public PageNotFoundException(String message) {
         super(message);
+        String page = ConfigurationManager.getProperty("path.page.error");
+
     }
 }
 
